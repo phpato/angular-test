592d924280baaeabf3c738eec3d98709e14adc87
patricio <patricio.cabrera@ingydev.cl>
2019-08-20 17:45:25 -0400

    se volvio a una version estable del codigo de los heroes, mas avances obsevables (angular)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
+import { Observable, of } from 'rxjs';
+import {HEROES} from '../mock-hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
